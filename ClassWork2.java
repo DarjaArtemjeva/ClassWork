f77570384a71af5fb9e680372ebfd5d0d73a9a97
DarjaArtemjeva <44693903+DarjaArtemjeva@users.noreply.github.com>
2019-10-09 19:11:52 +0300

    Add files via upload

---
+++ ClassWork2.java
@@ -0,0 +1,5 @@
+package lv.javaguru;
+
+public class ClassWork2 {
+
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+package lv.javaguru;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+
